c0855cec93e774c88e1c4a22b94d409c991995df
Tomás Lopez Hidalgo <tomas.lopezhidalgo@outlook.com>
2022-01-19 20:56:37 -0300

    Ignore target

--- target/debug/deps/frame_of_reference_remake.d
+++
@@ -1,5 +0,0 @@
-/private/var/git/frame-of-reference-remake/target/debug/deps/frame_of_reference_remake: src/main.rs
-
-/private/var/git/frame-of-reference-remake/target/debug/deps/frame_of_reference_remake.d: src/main.rs
-
-src/main.rs:
--- target/debug/frame-of-reference-remake.d
+++
@@ -1,1 +0,0 @@
-/private/var/git/frame-of-reference-remake/target/debug/frame-of-reference-remake: /private/var/git/frame-of-reference-remake/src/main.rs
